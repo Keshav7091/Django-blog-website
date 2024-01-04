407263a57d693cccffc708f5f9d2c5198b63986d
Keshav Kumar <keshavk1002003@gmail.com>
2024-01-04 17:28:17 +0530

    host on vercel.com

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+echo "Building the project..."
+
+echo "Make Migration..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Collect Static..."
 pip install -r requirements.txt 
-python3.11 manage.py collectstatic
+python3.11 manage.py collectstatic --noinput --clear
