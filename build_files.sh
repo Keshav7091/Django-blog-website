c04cac1b693e8c0db822c0b636647d72d1b3e627
Keshav Kumar <keshavk1002003@gmail.com>
2024-05-04 01:30:45 +0530

    some changes

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,4 @@
-echo "Building the project..."
-
-echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-echo "Collect Static..."
-pip install -r requirements.txt 
-python manage.py collectstatic --noinput --clear
+echo "BUILD START"
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput --clear
+echo "BUILD END"
